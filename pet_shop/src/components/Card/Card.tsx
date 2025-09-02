d5fb8914db282f8afb03b1aad3327f0a86a77d35
duarteeigor <duarteigor562@gmail.com>
2025-09-02 00:52:35 -0300

    feat: adicionando link na foto do card para detalhes

--- pet_shop/src/components/Card/Card.tsx
+++ pet_shop/src/components/Card/Card.tsx
@@ -1,17 +1,20 @@
-import type { ProductsProps } from "../../pages/home";
+import { Link } from "react-router";
 
 interface CardProps {
+    id: number
     title: string,
     cover: string,
     price: number,
     addToCart: () => void;
 }
 
-export function Card({ title, price, cover, addToCart }: CardProps) {
+export function Card({id, title, price, cover, addToCart }: CardProps) {
     return (
         <div>
             <section className="p-3 flex flex-col items-center gap-3 bg-[#fde8db] rounded-2xl">
-                <img className="w-full max-h-60 mb-3 object-contain p-2 rounded-2xl" src={cover} alt="img"></img>
+                <Link to={`/details/${id}`}>
+                <img className="w-full max-h-60 mb-3 object-contain p-2 rounded-2xl cursor-pointer" src={cover} alt="img"></img>
+                </Link>
                 <p className="font-medium line-clamp-2 px-2">{title}</p>
                 <strong className="text-zinc-800/90">{price.toLocaleString("pt-BR", {
                     style: "currency",
